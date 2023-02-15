43209f9a7af48ab0b82ff2f5cc16bd33ebd5bc1f
raffel080108 <62069283+raffel080108@users.noreply.github.com>
2023-02-15 02:08:45 +0100

    Updated copyright notices
    Updated copyright notices in all files to include license condition

--- src/main/java/me/raffel080108/altarcrafting/AltarCrafting.java
+++ src/main/java/me/raffel080108/altarcrafting/AltarCrafting.java
@@ -12,6 +12,18 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+    “Commons Clause” License Condition v1.0
+
+    The Software is provided to you by the Licensor under the License, as defined above, subject to the following condition.
+
+    Without limiting other conditions in the License, the grant of rights under the License will not include, and the License does not grant to you, right to Sell the Software.
+
+    For purposes of the foregoing, “Sell” means practicing any or all of the rights granted to you under the License to provide to third parties, for a fee or other consideration (including without limitation fees for hosting or consulting/ support services related to the Software), a product or service whose value derives, entirely or substantially, from the functionality of the Software.  Any license notice or attribution required by the License must also include this Commons Cause License Condition notice.
+
+    Software: AltarCrafting Plugin (https://github.com/raffel080108/AltarCrafting; https://www.spigotmc.org/resources/altarcrafting.107980/)
+    License: Apache License Version 2.0
+    Licensor: Raphael Roehrig (raffel080108)  
  */
 
 package me.raffel080108.altarcrafting;
