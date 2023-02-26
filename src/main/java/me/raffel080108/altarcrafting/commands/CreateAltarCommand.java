3f8df4d4a01ee0d353c69d3880dfddba66068b0a
raffel080108 <62069283+raffel080108@users.noreply.github.com>
2023-02-26 12:18:43 +0100

    Update of license notice in all files

--- src/main/java/me/raffel080108/altarcrafting/commands/CreateAltarCommand.java
+++ src/main/java/me/raffel080108/altarcrafting/commands/CreateAltarCommand.java
@@ -1,29 +1,8 @@
 /*
-   Copyright 2023 Raphael Roehrig (raffel080108)
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-    “Commons Clause” License Condition v1.0
-
-    The Software is provided to you by the Licensor under the License, as defined above, subject to the following condition.
-
-    Without limiting other conditions in the License, the grant of rights under the License will not include, and the License does not grant to you, right to Sell the Software.
-
-    For purposes of the foregoing, “Sell” means practicing any or all of the rights granted to you under the License to provide to third parties, for a fee or other consideration (including without limitation fees for hosting or consulting/ support services related to the Software), a product or service whose value derives, entirely or substantially, from the functionality of the Software.  Any license notice or attribution required by the License must also include this Commons Cause License Condition notice.
-
-    Software: AltarCrafting Plugin (https://github.com/raffel080108/AltarCrafting; https://www.spigotmc.org/resources/altarcrafting.107980/)
-    License: Apache License Version 2.0
-    Licensor: Raphael Roehrig (raffel080108)
+ *   Copyright 2023 Raphael Roehrig (alias "raffel080108"). All rights reserved.
+ *   Licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+ *   You may not use any content of this file or software except in compliance with the license
+ *   You can obtain a copy of the license here: https://creativecommons.org/licenses/by-nc/4.0/legalcode
  */
 
 package me.raffel080108.altarcrafting.commands;
